3d8080a940840840ce359f8bda00c7fa55c8f869
this Tom <wxh3300@outlook.com>
2016-06-29 12:07:03 +0800

    change
    change

--- project/js/bassPage.js
+++ project/js/bassPage.js
@@ -1,12 +1,21 @@
 window.onload=function () {
 	
 	document.getElementsByClassName('loading')[0].style.display='none';	
+	var tips=document.getElementsByClassName('tips')[0];
+	tips.style.right='10px';
+	var startTime=new Date(),
+	nowTime=new Date();
 	
-	setInterval(function () {
-		var mydate=new Date();
-		document.getElementsByClassName('time')[0].innerHTML=mydate.toLocaleString();
-	},1000)
-	
+	setInterval('mydate()',1000);
+	mydate=function () {
+		nowTime=new Date();
+		document.getElementsByClassName('time')[0].innerHTML=nowTime.toLocaleString();
+		
+		if (nowTime-startTime>4000) {
+		tips.style.opacity=0;
+	};
+	};
+
 	var body=document.getElementsByTagName('body')[0];
 	var mouse=utils.captureMouse(body);
 	window.addEventListener('mousemove',function mousePosition () {
